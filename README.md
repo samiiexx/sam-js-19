e6e3ef1066af4dcac5f265b71f6b6075a6b2e6bd
Favour Samuel <sfchidimma@gmail.com>
2021-08-02 21:12:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Theme Clock
+Project #19 in Brad Traversy's 50 Projects in 50 Days. This project is a clock. The hour, minute and seconds hands are styled with CSS. JS is used to power it and show the current time, in both analog and digital display, as well as date, and toggled between light and dark mode. 
+
+![Theme Clock](screenshot.jpg)
+
+## Built With
+- [HTML5](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5)
+- [CSS3](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+## Install
+```
+    $ git clone git@github.com:samiiexx/sam-js-19.git
+    $ cd main
+```  
+## Contributors
+- Favour Samuel (@samiiexx)
+
+## License
+Copyright (c) 2021 Favour Samuel.
+
+For enquiries please contact me at [sfchidimma@gmail.com](mailto:sfchidimma@gmail.com).
---
+++ index.js
@@ -0,0 +1,71 @@
+// Copyright 2021 sfchi
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const hourElement = document.querySelector('.hour')
+const minuteElement = document.querySelector('.minute')
+const secondElement = document.querySelector('.second')
+const timeElement = document.querySelector('.time')
+const dateElement = document.querySelector('.date')
+const toggleElement = document.querySelector('.toggle')
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+toggleElement.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+
+    if(html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    }
+    else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light mode'
+    }
+})
+
+function setTime() {
+   const time = new Date();
+   const month = time.getMonth() 
+   const day = time.getDay()
+   const date = time.getDate()
+   const hours = time.getHours() 
+   const hoursForClock = hours % 12
+   const minutes = time.getMinutes()
+   const seconds = time.getSeconds()
+   const ampm = hours >= 12 ? 'PM' : 'AM'
+
+   hourElement.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+
+   minuteElement.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+
+   secondElement.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+   timeElement.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+
+   dateElement.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime()
+
+setInterval(setTime, 1000)
+
+//********** Footer year ***********//
+//    var year = new Date().getFullYear();
+//    $("#footer-year").text(year);
